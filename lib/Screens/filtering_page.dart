66983389093283f8e7c12271bc2a738948178e8c
noranmohamed90 <144558368+noranmohamed90@users.noreply.github.com>
2024-10-18 22:25:06 +0300

    Add files via upload

---
+++ lib/Screens/filtering_page.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uc_mas_app/Screens/profile.dart'; 
+
+class FilteringPage extends StatefulWidget {
+  FilteringPage({super.key});
+
+  @override
+  State<FilteringPage> createState() => _FilteringPageState();
+}
+class _FilteringPageState extends State<FilteringPage> {
+  List<String> countries = ['مصر', 'السعودية', 'الإمارات', 'الأردن'];
+
+  Map<String, List<String>> countryCities = {
+    'مصر': [
+      'القاهرة', 'الجيزة', 'الإسكندرية', 'الدقهلية', 'البحر الأحمر', 'البحيرة', 'الفيوم',
+      'الغربية', 'الإسماعيلية', 'المنوفية', 'المنيا', 'القليوبية', 'الوادي الجديد', 'السويس',
+      'أسوان', 'أسيوط', 'بني سويف', 'بورسعيد', 'دمياط', 'الشرقية', 'جنوب سيناء', 'كفر الشيخ',
+      'مطروح', 'الأقصر', 'قنا', 'شمال سيناء', 'سوهاج'
+    ],
+    'السعودية': ['الرياض', 'جدة', 'مكة', 'المدينة', 'الدمام', 'الخبر', 'الطائف', 'القصيم'],
+    'الإمارات': ['أبو ظبي', 'دبي', 'الشارقة', 'عجمان', 'رأس الخيمة', 'الفجيرة', 'أم القيوين'],
+    'الأردن': ['عمان', 'الزرقاء', 'إربد', 'العقبة', 'مأدبا', 'الكرك', 'الطفيلة', 'جرش'],
+  };
+
+  String? selectedCountry = 'مصر';
+  String? selectedCity;
+
+  Stream<QuerySnapshot> getUsersFromFirestore() {
+    Query query = FirebaseFirestore.instance.collection('users');
+    
+    if (selectedCountry != null && selectedCountry!.isNotEmpty) {
+      query = query.where('country', isEqualTo: selectedCountry);
+    }
+
+    if (selectedCity != null && selectedCity!.isNotEmpty) {
+      query = query.where('governorate', isEqualTo: selectedCity);
+    }
+
+    return query.snapshots();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF3F4C5C),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: const Text(
+          "مجتمع UM Math",
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18),
+            child: DropdownButtonFormField(
+              borderRadius: BorderRadius.circular(8),
+              value: selectedCountry,
+              items: countries.map((String item) {
+                return DropdownMenuItem(
+                  value: item,
+                  child: Text(item),
+                );
+              }).toList(),
+              onChanged: (String? value) {
+                setState(() {
+                  selectedCountry = value;
+                  selectedCity = null; 
+                });
+              },
+              icon: const Icon(Icons.arrow_drop_down, color: Color(0xFF3F4C5C)),
+              decoration: const InputDecoration(labelText: 'اختر الدولة'),
+            ),
+          ),
+          if (selectedCountry != null)
+            Padding(
+              padding: const EdgeInsets.all(18),
+              child: DropdownButtonFormField(
+                borderRadius: BorderRadius.circular(8),
+                value: selectedCity,
+                items: countryCities[selectedCountry]!.map((String city) {
+                  return DropdownMenuItem(
+                    value: city,
+                    child: Text(city),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    selectedCity = value;
+                  });
+                },
+                icon: const Icon(Icons.arrow_drop_down, color: Color(0xFF3F4C5C)),
+                decoration: const InputDecoration(labelText: 'اختر المدينة',),
+                
+              ),
+            ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: getUsersFromFirestore(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    var users = snapshot.data!.docs;
+
+                    return Table(
+                      border: TableBorder.all(),
+                      columnWidths: const {
+                        0: IntrinsicColumnWidth(),
+                        1: FlexColumnWidth(),
+                        2: IntrinsicColumnWidth(),
+                      },
+                      children: [
+                        const TableRow(
+                          decoration: BoxDecoration(color: Color(0xFF3F4C5C)),
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text('الرتبة', style: TextStyle(color: Colors.white)),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text('الاسم', style: TextStyle(color: Colors.white)),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text('الإيميل', style: TextStyle(color: Colors.white)),
+                            ),
+                          ],
+                        ),
+
+                    
+                        for (var user in users)
+                          TableRow(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text((users.indexOf(user) + 1).toString()), 
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context, MaterialPageRoute(builder: (context)=>Profile()));
+                                   },
+                                  child: Text(user['name'] ?? 'No Name')), 
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(user['email'] ?? 'No Email'), 
+                              ),
+                            ],
+                          ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Screens/friends_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:uc_mas_app/Screens/profile.dart';
+
+// ignore: must_be_immutable
+class FrindsPage extends StatelessWidget {
+   FrindsPage({super.key});
+
+    List<Map<String, String>> Users = [
+  {'name': 'Alice Smith', 'email': 'alice@example.com'},
+  {'name': 'Bob Johnson', 'email': 'bob@example.com'},
+  {'name': 'Charlie Brown', 'email': 'charlie@example.com'},
+  {'name': 'David Lee', 'email': 'david@example.com'},
+  {'name': 'Eve Green', 'email': 'eve@example.com'},
+  {'name': 'Frank White', 'email': 'frank@example.com'},
+  {'name': 'Grace Black', 'email': 'grace@example.com'},
+  {'name': 'Hank Blue', 'email': 'hank@example.com'},
+  {'name': 'Ivy Red', 'email': 'ivy@example.com'},
+  {'name': 'Jack Yellow', 'email': 'jack@example.com'},
+];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF3F4C5C),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: const Text(
+          " الأصدقاء",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: 
+          Padding(padding: EdgeInsets.all(12),
+          child:  ListView.builder(
+              itemCount: Users.length, 
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.all(12),
+                  child: ListTile(
+                    leading: const CircleAvatar(
+                      backgroundColor: Color(0xFF3F4C5C),
+                      child: Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      ),
+                    ),
+                    title: InkWell(
+                      onTap: (){
+                        
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>Profile()));
+                      },
+                      child: Text(
+                        Users[index]['name']!,
+                        style: const TextStyle(
+                          color: Color(0xFF3F4C5C),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+          ));
+  }),
+      ),
+    );
+  
+  }
+}
